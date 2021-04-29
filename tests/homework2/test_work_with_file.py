3e29a86f5477c8864dc5aeb94a9651a30a78878a
Artem <artem.potashov@bk.ru>
2021-04-29 03:59:49 +0300

    add cleanup

--- tests/homework2/test_work_with_file.py
+++ tests/homework2/test_work_with_file.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from homework2.task1 import task
@@ -15,18 +17,24 @@ class TestWorkWithFile:
 
     def test_is_exist_file(self, create_file):
         assert create_file.name == "test_file.txt"
+        os.remove(create_file.name)
 
     def test_find_10_longest_unique_words(self, create_file):
         assert task.get_longest_diverse_words(create_file.name) == ["Jetzt", "hier"]
+        os.remove(create_file.name)
 
     def test_find_rarest_symbol(self, create_file):
         assert task.get_rarest_char(create_file.name) == "»"
+        os.remove(create_file.name)
 
     def test_calculate_count_punctuation(self, create_file):
         assert task.count_punctuation_chars(create_file.name) == 2
+        os.remove(create_file.name)
 
     def test_calculate_count_symbols_unicode_escape(self, create_file):
         assert task.count_non_ascii_chars(create_file.name) == 2
+        os.remove(create_file.name)
 
     def test_find_common_symbols_unicode_escape(self, create_file):
         assert task.get_most_common_non_ascii_char(create_file.name) == "»"
+        os.remove(create_file.name)
