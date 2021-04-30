69c7c79f011dd9006957e86a1332e44885b78ab3
Artem <artem.potashov@bk.ru>
2021-04-30 11:37:44 +0300

    add tests

--- tests/homework2/test_custom_range.py
+++ tests/homework2/test_custom_range.py
@@ -7,68 +7,52 @@ from homework2.task5.task import custom_range
 
 class TestCustomRange:
     def test_ascii_range_arg_ascii_low_stop(self):
-        assert custom_range(string.ascii_lowercase, "g") == [
-            "a",
-            "b",
-            "c",
-            "d",
-            "e",
-            "f",
-        ]
+        assert custom_range(string.ascii_lowercase, "g") == ["a", "b", "c", "d", "e", "f"]
+
+    def test_ascii_range_arg_ascii_low_stop_equal_start(self):
+        assert custom_range(string.ascii_lowercase, "a") == []
 
     def test_ascii_range_arg_ascii_up_stop(self):
-        assert custom_range(string.ascii_uppercase, "G") == [
-            "A",
-            "B",
-            "C",
-            "D",
-            "E",
-            "F",
-        ]
+        assert custom_range(string.ascii_uppercase, "G") == ["A", "B", "C", "D", "E", "F"]
+
+    def test_ascii_range_arg_ascii_up_stop_equal_start(self):
+        assert custom_range(string.ascii_uppercase, "A") == []
 
     def test_ascii_range_arg_ascii_low_start_stop(self):
-        assert custom_range(string.ascii_lowercase, "g", "p") == [
-            "g",
-            "h",
-            "i",
-            "j",
-            "k",
-            "l",
-            "m",
-            "n",
-            "o",
-        ]
+        assert custom_range(string.ascii_lowercase, "g", "p") == ["g", "h", "i", "j", "k", "l", "m", "n", "o"]
 
     def test_ascii_range_arg_ascii_up_start_stop(self):
-        assert custom_range(string.ascii_uppercase, "G", "P") == [
-            "G",
-            "H",
-            "I",
-            "J",
-            "K",
-            "L",
-            "M",
-            "N",
-            "O",
-        ]
+        assert custom_range(string.ascii_uppercase, "G", "P") == ["G", "H", "I", "J", "K", "L", "M", "N", "O"]
 
-    def test_ascii_range_arg_ascii_low_start_stop_step(self):
-        assert custom_range(string.ascii_lowercase, "p", "g", -2) == [
-            "p",
-            "n",
-            "l",
-            "j",
-            "h",
-        ]
+    def test_ascii_range_arg_ascii_low_start_stop_equal(self):
+        assert custom_range(string.ascii_lowercase, "a", "a") == []
 
-    def test_ascii_range_arg_ascii_up_start_stop_step(self):
-        assert custom_range(string.ascii_uppercase, "P", "G", -2) == [
-            "P",
-            "N",
-            "L",
-            "J",
-            "H",
-        ]
+    def test_ascii_range_arg_ascii_up_start_stop_equal(self):
+        assert custom_range(string.ascii_uppercase, "A", "A") == []
+
+    def test_ascii_range_arg_ascii_low_start_more_stop(self):
+        assert custom_range(string.ascii_lowercase, "b", "a") == []
+
+    def test_ascii_range_arg_ascii_up_start_more_stop(self):
+        assert custom_range(string.ascii_uppercase, "B", "A") == []
+
+    def test_ascii_range_arg_ascii_low_start_stop_positive_step(self):
+        assert custom_range(string.ascii_lowercase, "g", "p", 2) == ['g', 'i', 'k', 'm', 'o']
+
+    def test_ascii_range_arg_ascii_up_start_stop_positive_step(self):
+        assert custom_range(string.ascii_uppercase, "G", "P", 2) == ['G', 'I', 'K', 'M', 'O']
+
+    def test_ascii_range_arg_ascii_low_start_stop_positive_step_more_then_stop(self):
+        assert custom_range(string.ascii_lowercase, "g", "p", 100) == ['g']
+
+    def test_ascii_range_arg_ascii_up_start_stop_positive_step_more_then_stop(self):
+        assert custom_range(string.ascii_uppercase, "G", "P", 100) == ['G']
+
+    def test_ascii_range_arg_ascii_low_start_stop_negative_step(self):
+        assert custom_range(string.ascii_lowercase, "p", "g", -2) == ["p", "n", "l", "j", "h"]
+
+    def test_ascii_range_arg_ascii_up_start_stop_negative_step(self):
+        assert custom_range(string.ascii_uppercase, "P", "G", -2) == ["P", "N", "L", "J", "H"]
 
     def test_ascii_range_wrong_arguments(self):
         with pytest.raises(ValueError) as wr_a:
@@ -82,3 +66,10 @@ class TestCustomRange:
             custom_range("g")
         exception_msg = tp_e.value.args[0]
         assert exception_msg == "no argument encoding"
+
+    def test_step_0(self):
+        with pytest.raises(ValueError) as e:
+            custom_range(string.ascii_lowercase, 'a', 'p', 0)
+            custom_range(string.ascii_lowercase, 'A', 'P', 0)
+        exception_msg = e.value.args[0]
+        assert exception_msg == "range() arg 3 must not be zero"
--- tests/homework2/test_work_with_file.py
+++ tests/homework2/test_work_with_file.py
@@ -6,38 +6,34 @@ from homework2.task1 import task
 
 
 class TestWorkWithFile:
+
     @pytest.fixture
-    def create_file(self):
+    def create_file(self, tmp_path):
+        file_path = os.path.join(tmp_path, 'test_file.txt')
         u_e_word = r"\u00bb"
         u_e_word1 = r"\u00ab"
         test_data = "Jetzt-\nderisqse hier"
-        with open("test_file.txt", "w") as f_o:
+        with open(file_path, "w") as f_o:
             f_o.writelines(f"{u_e_word}{test_data}{u_e_word1}")
-            return f_o
+            return file_path
 
     def test_is_exist_file(self, create_file):
-        assert create_file.name == "test_file.txt"
-        os.remove(create_file.name)
+        assert os.path.isfile(create_file)
 
     def test_find_10_longest_unique_words(self, create_file):
-        assert task.get_longest_diverse_words(create_file.name) == [
+        assert task.get_longest_diverse_words(create_file) == [
             "Jetztderisqse",
             "hier",
         ]
-        os.remove(create_file.name)
 
     def test_find_rarest_symbol(self, create_file):
-        assert task.get_rarest_char(create_file.name) == "»"
-        os.remove(create_file.name)
+        assert task.get_rarest_char(create_file) == "»"
 
     def test_calculate_count_punctuation(self, create_file):
-        assert task.count_punctuation_chars(create_file.name) == 3
-        os.remove(create_file.name)
+        assert task.count_punctuation_chars(create_file) == 3
 
     def test_calculate_count_symbols_unicode_escape(self, create_file):
-        assert task.count_non_ascii_chars(create_file.name) == 2
-        os.remove(create_file.name)
+        assert task.count_non_ascii_chars(create_file) == 2
 
     def test_find_common_symbols_unicode_escape(self, create_file):
-        assert task.get_most_common_non_ascii_char(create_file.name) == "»"
-        os.remove(create_file.name)
+        assert task.get_most_common_non_ascii_char(create_file) == "»"
