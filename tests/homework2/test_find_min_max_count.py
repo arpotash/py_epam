123a7263b934287980d339032e15bc87181ca91a
Artem <artem.potashov@bk.ru>
2021-05-13 15:26:46 +0300

    add tests

--- tests/homework2/test_find_min_max_count.py
+++ tests/homework2/test_find_min_max_count.py
@@ -1,11 +1,9 @@
-import pytest
-
 from homework2.task2.task import major_and_minor_elem
 
 
 class TestMinMaxCount:
-    @pytest.mark.parametrize(
-        "lst, excepted_values", [([3, 2, 3], (3, 2)), ([2, 2, 2, 2], (2, 2))]
-    )
-    def test_find_min_max(self, lst, excepted_values):
-        assert major_and_minor_elem(lst) == excepted_values
+    def test_find_min_max(self):
+        assert major_and_minor_elem([3, 2, 3]) == (3, 2)
+
+    def test_no_min(self):
+        assert major_and_minor_elem([3, 3, 3]) == (3, None)
