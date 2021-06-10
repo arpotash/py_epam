10bc2fe0af578e732a01c4e866a86161216cc4ec
arpotash <artem.potashov@bk.ru>
2021-06-10 16:15:18 +0300

    add homework 10

---
+++ homework10/school/mainapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Teacher, Student, Homework, HomeworkResult
+# Register your models here.
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Homework)
+admin.site.register(HomeworkResult)
